bffe78d54e545c3ffe0bd883ce32e8277b3aaecb
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-03-18 00:27:30 +0200

    добавил кнопку в футере для модалки

--- js/modal.js
+++ js/modal.js
@@ -3,11 +3,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtnBottom: document.querySelector('[data-modal-open-bottom]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnBottom.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
